90334da16b8e35a31f65b682e7917d80b3fcece2
Daniel Miller <daniel@matv.io>
2024-02-22 04:09:10 -0500

    Updated navbar to properly handle auth

--- src/app/components/NavbarRookery.tsx
+++ src/app/components/NavbarRookery.tsx
@@ -9,68 +9,113 @@ import {
   Dropdown,
   DropdownMenu,
   Avatar,
+  Button,
+  Image,
 } from "@nextui-org/react";
+import {
+  useSession,
+  signOut,
+  ClientSafeProvider,
+  signIn,
+} from "next-auth/react";
+import {
+  DiscordIcon,
+  EventRegistrationIcon,
+  LoginIcon,
+  LogoutIcon,
+  ProfileIcon,
+} from "./Icons";
 
 // import {AcmeLogo} from "./AcmeLogo.jsx";
 
-export default async function NavbarRookery() {
-  
-
+export default function NavbarRookery({
+  loginProvider,
+}: {
+  loginProvider: ClientSafeProvider;
+}) {
+  const { data: session, status } = useSession();
 
   return (
-    <Navbar>
+    <Navbar className="absolute">
       <NavbarBrand>
-        {/* <AcmeLogo /> */}
-        <p className="font-bold text-inherit">ACME</p>
+        <Link href="/" className="flex gap-4 font-bold text-inherit">
+          <Avatar
+            isBordered
+            className="transition-transform"
+            color="default"
+            size="sm"
+            src="logo.webp"
+          />
+          <p>RPI Chess Club</p>
+        </Link>
       </NavbarBrand>
-
       <NavbarContent className="hidden gap-4 sm:flex" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
+          <Link color="foreground" href="/">
+            Home
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page" color="secondary">
-            Customers
-          </Link>
-        </NavbarItem>
+        {/* use isActive and primary */}
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+          <Link color="foreground" href="/events">
+            Events
           </Link>
         </NavbarItem>
       </NavbarContent>
 
       <NavbarContent as="div" justify="end">
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="gap-2 h-14">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+        {status === "authenticated" && session && (
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="primary"
+                name={session?.user.name || ""}
+                size="sm"
+                src={session?.user.image || ""}
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profileinfo" className="gap-2 h-14">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{session?.user.name}</p>
+              </DropdownItem>
+              <DropdownItem
+                href="/profile"
+                key="profile"
+                startContent={<ProfileIcon />}
+              >
+                My Profile
+              </DropdownItem>
+              <DropdownItem
+                href="/registration"
+                key="registration"
+                startContent={<EventRegistrationIcon />}
+              >
+                Event Registration
+              </DropdownItem>
+              <DropdownItem
+                key="logout"
+                color="danger"
+                startContent={<LogoutIcon />}
+                onClick={() => signOut()}
+              >
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        )}
+        {status === "unauthenticated" && (
+          <Button
+            endContent={<DiscordIcon />}
+            as={Link}
+            onClick={() => signIn(loginProvider.id)}
+          >
+            Sign in with Discord
+          </Button>
+        )}
       </NavbarContent>
     </Navbar>
   );
