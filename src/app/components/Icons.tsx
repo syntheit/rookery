4af15ee141eda802fd504c014a2ff754e6bb97a2
Daniel Miller <daniel@matv.io>
2024-04-24 20:07:57 -0400

    Added edit icon

--- src/app/components/Icons.tsx
+++ src/app/components/Icons.tsx
@@ -262,3 +262,29 @@ export const WarningIcon = ({
     <path d="M12 16h.01" />
   </svg>
 );
+
+export const EditIcon = ({
+  size = 24,
+  width,
+  height,
+  color = "currentColor",
+  ...props
+}: IconSvgProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke={color}
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height={size || height}
+    width={size || width}
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+    <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+    <path d="M16 5l3 3" />
+  </svg>
+);
